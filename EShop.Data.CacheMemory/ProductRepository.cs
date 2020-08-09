bdc3fabc513fdaa6c4a21b6abc92373ac528afb6
Shujat Ali <shuja4you@hotmail.com>
2020-08-09 22:57:34 +0100

    creating Models and caching memory

---
+++ EShop.Data.CacheMemory/ProductRepository.cs
@@ -0,0 +1,60 @@
+﻿using EShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Data.CacheMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+
+            if (productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+        public Product Find(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+
+        }
+    }
+}
